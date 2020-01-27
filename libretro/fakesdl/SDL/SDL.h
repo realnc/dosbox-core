56215367b47e99d48f8bce140ae21e0810e61592
Nikos Chantziaras <realnc@gmail.com>
2020-01-28 01:50:36 +0200

    Add SDL_thread and SDL_endian to fakesdl and clean it up a little

--- libretro/fakesdl/SDL/SDL.h
+++ libretro/fakesdl/SDL/SDL.h
@@ -1,53 +1,32 @@
-#ifndef SDL_FAKE_
-#define SDL_FAKE_
-
-#include <string.h>
-#include <stdint.h>
-#include <sys/time.h>
-#include <unistd.h>
-
-#include <retro_miscellaneous.h>
+#pragma once
+#include "SDL_endian.h"
+#include "SDL_thread.h"
+#include "SDL_timer.h"
+#include <cstdint>
 
 #ifdef VITA
 #include <psp2/kernel/threadmgr.h>
 #endif
 
-#include <retro_timers.h>
-
-typedef uint8_t Uint8;
-typedef uint16_t Uint16;
-typedef uint32_t Uint32;
+using Uint8 = std::uint8_t;
+using Uint16 = std::uint16_t;
+using Uint32 = std::uint32_t;
 
 // This is used in src/ints/bios_keyboard.cpp as a workaround for caps/num lock on old SDL versions.
 #define SDL_VERSION_ATLEAST(...) 0
 
-// These are 'used' in src/dos/cdrom_image.cpp and src/dos/cdrom_ioctl_win32.cpp.
-// Really they are meant to synchronize with an audio thread, but
-// that isn't used in libreto so there is no need for these.
-typedef int SDL_mutex;
-#define SDL_CreateMutex() 0
-#define SDL_DestroyMutex(...)
-#define SDL_mutexV(...)
-#define SDL_mutexP(...)
-
-// TIMER
-inline void SDL_Delay(unsigned ms)
+inline constexpr auto SDL_CDNumDrives() noexcept -> int
 {
-   retro_sleep(ms);
+    return 0;
 }
 
-inline unsigned SDL_GetTicks()
+inline constexpr const char* SDL_CDName(const int /*drive*/)
 {
-    struct timeval t;
-    gettimeofday(&t, 0);
-    
-    return (t.tv_sec * 1000) + (t.tv_usec / 1000);
+    return "NONE";
 }
 
-// CD
-#define SDL_CDNumDrives() 0
-#define SDL_CDName(x) "NONE"
-struct SDL_CD {int dummy;};
+struct SDL_CD
+{ };
 
 /** @name Frames / MSF Conversion Functions
  *  Conversion functions from frames to Minute/Second/Frames and vice versa
@@ -64,5 +43,3 @@ struct SDL_CD {int dummy;};
 }
 #define MSF_TO_FRAMES(M, S, F)	((M)*60*CD_FPS+(S)*CD_FPS+(F))
 /*@}*/
-
-#endif

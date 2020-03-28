409ea9d33159a27fab7d58f9fa15e4dac6ae0913
Nikos Chantziaras <realnc@gmail.com>
2020-03-28 14:37:59 +0200

    Fix RA shaders not working on non-C numeric locales
    @DamsRoxxx discovered that libinstpatch is hijacking the current locale:
    
      https://github.com/realnc/dosbox-core/issues/7
    
    Implement a workaround that restores the current locale after
    libinstpatch changes it.

--- libretro/midi_fluidsynth.cpp
+++ libretro/midi_fluidsynth.cpp
@@ -4,7 +4,25 @@
 #include "control.h"
 #include "libretro_dosbox.h"
 #include "setup.h"
+#include <clocale>
 #include <string_view>
+
+// Fluidsynth uses libinstpatch which as of v1.1.3 has the nasty behavior of changing the locale
+// under our nose. We remember and restore the current locale whenever this could happen.
+//
+// https://github.com/realnc/dosbox-core/issues/7
+// https://github.com/swami/libinstpatch/issues/37
+class LocaleGuard final
+{
+public:
+    ~LocaleGuard()
+    {
+        setlocale(LC_ALL, locale_.c_str());
+    }
+
+private:
+    std::string locale_ = setlocale(LC_ALL, nullptr);
+};
 
 MidiHandlerFluidsynth MidiHandlerFluidsynth::instance_;
 
@@ -60,6 +78,8 @@ void init_fluid_dosbox_settings(Section_prop& secprop)
 
 auto MidiHandlerFluidsynth::Open(const char* const /*conf*/) -> bool
 {
+    LocaleGuard locale_guard;
+
     Close();
 
     auto* section = static_cast<Section_prop*>(control->GetSection("midi"));

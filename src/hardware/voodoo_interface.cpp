9d97bf2571805f30a6b5f0acce623e6be20a8636
Nikos Chantziaras <realnc@gmail.com>
2020-05-08 08:52:29 +0300

    Fix 3dfx passing frame time instead of FPS to RENDER_SetSize

--- src/hardware/voodoo_interface.cpp
+++ src/hardware/voodoo_interface.cpp
@@ -213,7 +213,7 @@ static void Voodoo_UpdateScreen(void) {
 		if (v->ogl) {
 			v->ogl_dimchange = false;
 		} else {
-			RENDER_SetSize(v->fbi.width, v->fbi.height, 16, vdraw.vfreq, 4.0/3.0, false, false);
+			RENDER_SetSize(v->fbi.width, v->fbi.height, 16, 1000.0f / vdraw.vfreq, 4.0/3.0, false, false);
 		}
 
 		Voodoo_VerticalTimer(0);

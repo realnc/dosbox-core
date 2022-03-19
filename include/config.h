2f7fc682fb87a5641dd9a628f40bbe60a272b485
Nikos Chantziaras <realnc@gmail.com>
2022-03-19 15:05:22 +0200

    Change dosbox VERSION macro from "SVN-libretro" to "core"

--- include/config.h
+++ include/config.h
@@ -91,7 +91,7 @@
 ///////////////////
 
 /* Version number of package */
-#define VERSION "SVN-libretro"
+#define VERSION "core"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */

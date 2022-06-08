34e1e84737ed22423ed01fa01311ef48dba4d35d
Nikos Chantziaras <realnc@gmail.com>
2022-06-08 18:32:06 +0300

    Rename x86_new dynarec to x86_generic
    Because that's what it actually is. The "new" in the name implies it's
    better, while actually it's worse (slower.)

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -12,7 +12,7 @@ else ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
 else ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
     WITH_DYNAREC := arm64
 else ifeq ($(TARGET_ARCH_ABI), x86)
-    WITH_DYNAREC := x86_new
+    WITH_DYNAREC := x86_generic
 else ifeq ($(TARGET_ARCH_ABI), x86_64)
     WITH_DYNAREC := x86_64
 else ifeq ($(TARGET_ARCH_ABI), mips)
